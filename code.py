319e561bc0f39e81eb27ed90a2776d7e20cfec5a
AnushkaLovesBrownies <96715977+AnushkaLovesBrownies@users.noreply.github.com>
2022-01-12 19:25:44 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("data.csv")
+
+fig = px.scatter(df,   
+    x="student_id",
+    y="level",
+    color="attempt"
+            )
+fig.show()
